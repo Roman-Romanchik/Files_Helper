e1511bdcbe672353abd3c91bdd24de277420dc51
Roman-Romanchik <kiri_main@mail.ru>
2025-04-24 19:22:50 +0300

    Add controller Need add some methods for all buttons

--- src/main/java/com/example/Files/Helper/App.java
+++ src/main/java/com/example/Files/Helper/App.java
@@ -1,21 +1,13 @@
 package com.example.Files.Helper;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.geometry.HPos;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
-import javafx.geometry.VPos;
-import javafx.scene.control.Tooltip;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.text.Font;
-import javafx.util.Duration;
+import javafx.scene.Parent;
 
 public class App extends Application {
    
@@ -26,73 +18,17 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("Files Helper");
+		
+		Parent root;
+		try{
+			root = FXMLLoader.load(getClass().getResource("/main_View.fxml"));
+			Scene scene = new Scene(root);
         
-		Tooltip tooltipForButtonsCreateFolders = new Tooltip("create folders");
-		tooltipForButtonsCreateFolders.setShowDelay(Duration.ZERO);
-		
-		Tooltip tooltipForButtonAddFiles = new Tooltip("Add files");
-		tooltipForButtonAddFiles.setShowDelay(Duration.ZERO);
-		
-        Label labelChooseAction = new Label("Choose your action");
-		labelChooseAction.setFont(Font.font(20));
-		GridPane.setHalignment(labelChooseAction, HPos.CENTER);
-		GridPane.setValignment(labelChooseAction, VPos.CENTER);
-		
-        Button buttonCreateFolders = new Button("Create folders");
-		buttonCreateFolders.setFont(Font.font(15));
-		GridPane.setHalignment(buttonCreateFolders, HPos.RIGHT);
-		GridPane.setValignment(buttonCreateFolders, VPos.CENTER);
-		buttonCreateFolders.setMaxWidth(120);
-		buttonCreateFolders.setMaxHeight(35);
-		buttonCreateFolders.setTooltip(tooltipForButtonsCreateFolders);
-		
-        Button buttonAddFiles = new Button("Add files");
-		buttonAddFiles.setFont(Font.font(15));
-		GridPane.setHalignment(buttonAddFiles, HPos.LEFT);
-		GridPane.setValignment(buttonAddFiles, VPos.CENTER);
-		buttonAddFiles.setMaxWidth(120);
-		buttonAddFiles.setMaxHeight(35);
-		buttonAddFiles.setTooltip(tooltipForButtonAddFiles);
-        
-        
-        
-        GridPane root = new GridPane();
-		
-		
-		ColumnConstraints firstColumn = new ColumnConstraints(200, 200, Double.MAX_VALUE);
-		ColumnConstraints secondColumn = new ColumnConstraints(200, 200, Double.MAX_VALUE);
-		ColumnConstraints thirdColumn = new ColumnConstraints(200, 200, Double.MAX_VALUE);
-		
-		RowConstraints firstRow = new RowConstraints(50);
-		RowConstraints secondRow = new RowConstraints(50);
-		
-		firstColumn.setHgrow(Priority.ALWAYS);
-		secondColumn.setHgrow(Priority.ALWAYS);
-		thirdColumn.setHgrow(Priority.ALWAYS);
-		
-		root.getColumnConstraints().add(firstColumn);
-		root.getColumnConstraints().add(secondColumn);
-		root.getColumnConstraints().add(thirdColumn);
-		
-		root.getRowConstraints().add(firstRow);
-		root.getRowConstraints().add(secondRow);
-		
-        root.add(labelChooseAction, 1, 0);
-        root.add(buttonCreateFolders, 0, 1);
-        root.add(buttonAddFiles, 2, 1);
-        
-        buttonCreateFolders.setOnAction(event ->{
-			CreateFolders createFolders = new CreateFolders();
-			createFolders.openCreateFoldersMenu();
-		});
-        buttonAddFiles.setOnAction(event -> {
-			AddFilesToFolder addFilesToFolder = new AddFilesToFolder();
-			addFilesToFolder.openAddFilesMenu();
-		});
-		
-        Scene scene = new Scene(root, 600, 400);
-        
-        stage.setScene(scene);
-        stage.show();
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException ex) {
+			Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		     
     }
 }
